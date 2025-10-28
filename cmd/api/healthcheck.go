71625bc3c6e6c14577d641d468dd9a0989d38762
Bombbomb <hongbangltv@gmail.com>
2025-10-28 20:37:37 +0700

    update: healthcheckHandler with json encoding

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,33 @@
 package main 
 import(
-	"fmt"
+	// "fmt"
 	"net/http"
+	"encoding/json"
 )
 
-func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request){
+// func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request){
 
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+// 	js := `{"status": "available", "environment": %q, "version": %q}`
+// 	js = fmt.Sprintf(js, app.config.env, version)
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.Write([]byte(js))
+// }
+
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request){
+	data := map[string]string{
+		"status": "available",
+		"environment": app.config.env, 
+		"version": version, 
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return 
+	}
+
+	js = append(js, '\n')
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+	w.Write(js)
 }

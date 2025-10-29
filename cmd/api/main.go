1b195458feed1de2025df1d1cb1f94b32e3b2cf7
Bombbomb <hongbangltv@gmail.com>
2025-10-29 18:36:05 +0700

    setting up postgresql in local and connecting to postgreSQL

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,9 @@ import (
 	"net/http"
 	"os"
 	"time"
+	"context"
+	"database/sql"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -13,6 +16,9 @@ const version = "1.0.0"
 type config struct {
 	port int 
 	env string 
+	db struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -25,10 +31,27 @@ func main(){
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+	
 	flag.Parse() 
 
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// Call the openDB() helper function (see below) to create the connection pool,
+	// passing in the config struct. If this returns an error, we log it and exit the
+	// application immediately.
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error("unable to connect to database", "error", err)
+		os.Exit(1)
+	}
+	
+	// Defer a call to db.Close() so that the connection pool is closed before the
+	// main() function exits.
+	defer db.Close()
+	logger.Info("Database connection pool established")
 
 	app := &application{
 		config: cfg, 
@@ -47,9 +70,27 @@ func main(){
 		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError), 
 	}
 	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
-	err := srv.ListenAndServe() 
+	err = srv.ListenAndServe() 
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+}
+
+func openDB(cfg config) (*sql.DB, error){
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err 
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel() 
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err 
+	}
+
+	return db, nil
+
 }

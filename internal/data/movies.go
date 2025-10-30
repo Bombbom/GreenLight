7f10a74b46eebd46f3aed96fcdac81406732c594
Bombbomb <hongbangltv@gmail.com>
2025-10-30 11:20:43 +0700

    add: CRUD for movies models

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,6 +3,8 @@ import (
 	"time"
 	"greenlight.clone/internal/validator"
 	"database/sql"
+	"github.com/lib/pq" 
+	"errors"
 )
 
 type Movie struct {
@@ -37,16 +39,85 @@ type MovieModel struct {
 }
 
 func (m MovieModel) Insert(movie *Movie) error {
-	return nil 
+	// return nil 
+	query := `
+		INSERT INTO movies (title, year, runtime, genres)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, created_at, version`
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m MovieModel) Get(id int64) (*Movie, error){
-	return nil, nil 
+	// return nil, nil 
+	if id < 1 {
+		return nil, ErrRecordNotFound 
+	}
+
+	query := `
+		SELECT id, created_at, title, year, runtime, genres, version 
+		FROM movies 
+		WHERE id = $1`
+	var movie Movie 
+	err := m.DB.QueryRow(query, id).Scan(
+		&movie.ID, 
+		&movie.CreatedAt, 
+		&movie.Title, 
+		&movie.Year, 
+		&movie.Runtime, 
+		pq.Array(&movie.Genres), 
+		&movie.Version, 
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound 
+		default:
+			return nil, err 
+
+		}
+	}
+	return &movie, nil 
 }
 func (m MovieModel) Update(movie *Movie) error {
-	return nil 
+	// return nil 
+	query := `
+		UPDATE movies 
+		SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+		WHERE id = $5 
+		RETURNING version`
+	args := []any{
+		movie.Title, 
+		movie.Year, 
+		movie.Runtime, 
+		pq.Array(movie.Genres), 
+		movie.ID, 
+	}
+	return m.DB.QueryRow(query, args...).Scan(&movie.Version) 
+	
 }
 
 func (m MovieModel) Delete(id int64) error {
-	return nil 
+	// return nil 
+	if id < 1 {
+		return ErrRecordNotFound 
+	}
+
+	query := `
+		DELETE FROM movies 
+		WHERE id = $1`
+	result, err := m.DB.Exec(query, id)
+	if err != nil {
+		return err 
+	}
+
+	rowAffected, err := result.RowsAffected() 
+	if err != nil {
+		return err 
+	}
+
+	if rowAffected == 0 {
+		return ErrRecordNotFound 
+	}
+	return nil
 }

11354a4fb63a8a0eba8ca6dd44aa40a0d329e704
Bombbomb <hongbangltv@gmail.com>
2025-10-30 21:51:44 +0700

    listing data, filtering lists

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -8,11 +8,13 @@ import (
 	"fmt"
 	"io"
 	"strings"
+	"net/url"
+	"greenlight.clone/internal/validator"
 )
 
 type envelope map[string]any 
 
-func (app *application) readIDParam(r *http.Request) (int64, error){
+func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
 	if err != nil || id < 1{
@@ -86,4 +88,37 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 		return errors.New("Body must only contain a single JSON value")
 	}
 	return nil 
+}
+
+
+func (app *application) readString(qs url.Values, key string, defaultValue string) string {
+	s := qs.Get(key)
+	if s == ""  {
+		return defaultValue 
+	}
+	return s 
+
+}
+
+func (app *application) readCSV(qs url.Values, key string, defaultValue []string) []string{
+	csv := qs.Get(key)
+	if csv == ""{
+		return defaultValue 
+	}
+	return strings.Split(csv, ",")
+
+}
+
+
+func (app *application) readInt(qs url.Values, key string, defaultValue int, v *validator.Validator) int {
+	s := qs.Get(key)
+	if s == "" {
+		return defaultValue 
+	}
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		v.AddError(key, "must be an integer value")
+		return defaultValue 
+	}
+	return i 
 }

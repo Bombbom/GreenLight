5620e39c0f8d57041b7e66c3e9a8db17e09db39b
Bombbomb <hongbangltv@gmail.com>
2025-10-30 22:29:59 +0700

    add: implementing sorting

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"greenlight.clone/internal/validator" 
+	"strings"
 )
 
 type Filters struct {
@@ -19,6 +20,23 @@ func ValidateFilters(v *validator.Validator, f Filters){
 
 	v.Check(validator.PermittedValue(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
 
+}
+
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-"){
+		return "DESC" 
+	}
+	return "ASC"
 }
 
 
